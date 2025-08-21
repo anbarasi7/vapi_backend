20114ab6f2f93e4a4f08e6cec21ba6b1144ba313
sumit-realtyai <sumit@realtyai.net>
2025-08-21 18:09:07 +0530

    custom transcript

--- controllers/callReportController.js
+++ controllers/callReportController.js
@@ -13,7 +13,7 @@ const backend_url = process.env.BACKEND_URL || 'https://backend-robotics-voice-a
 export const createAssistant = async (req, res) => {
   const { childName, customPrompt , vapiKey , prompt, toyName } = req.body;
   const VAPI_API_KEY = vapiKey || "b2047282-7b8a-421b-b33d-7abfdbdddcfd";
-  
+  console.log("VAPI API Key:", VAPI_API_KEY);
   let finalPrompt = `You are a kid assistant, who helps engage kids in a fun playful manner.
    Please be concise in your responses. Use very simple language that kids can understand and use short sentences. 
   Make sure to follow these instruction while replying: ${customPrompt || "Be friendly and helpful."}`;
